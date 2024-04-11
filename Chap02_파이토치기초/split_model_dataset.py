2e85540517609df481e0e2fe74b6bef8084be881
yoonyoojin <dbsdbwls0602@naver.com>
2024-04-11 21:13:27 +0900

    비선형 회귀 학습 및 모델 평가

--- Chap02_파이토치기초/split_model_dataset.py
+++ Chap02_파이토치기초/split_model_dataset.py
@@ -50,4 +50,70 @@ class CustomDataset(Dataset):
     def __len__(self):
         return self.length
     
+
+#사용자 정의 모델
+class CustomModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer = nn.Linear(2,1)
+
+    def forward(self,x):
+        x = self.layer(x)
+        return x
     
+
+#사용자 정의 데이터세트 및 데이터로더
+train_dataset = CustomDataset('../datasets/non_linear.csv')
+train_dataloader = DataLoader(train_dataset, batch_size=128, shuffle=True, drop_last=True)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = CustomModel().to(device)
+criterion = nn.MSELoss().to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.0001)
+
+for epoch in range(10000):
+    cost = 0.0
+
+    for x, y in train_dataloader:
+        x = x.to(device)
+        y = y.to(device)
+
+        output = model(x)
+        loss = criterion(output, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        cost += loss
+
+    cost = cost / len(train_dataloader)
+
+    if (epoch +1) % 1000 == 0:
+        print(f"epoch : {epoch +1:4d}, Model: {list(model.parameters())}, Cost: {cost:.3f}")
+
+
+ #%%
+"""
+ 모델 평가
+"""   
+
+with torch.no_grad(): #자동 미분 기능을 사용하지 않도록 설정해 메모리 사용량을 줄임
+    model.eval() # 평가 모드로 반환(특정 계층에서는 평가 모드와 학습 모드가 다름)
+    inputs = torch.FloatTensor([
+        [1**2,1],
+        [5**2,5],
+        [11**2,11]
+    ]).to(device)
+
+    outputs = model(inputs)
+    print(outputs)
+
+ #모델 저장
+torch.save(
+     model, "../models/model.pt"
+ )
+
+torch.save(
+    model.state_dict(), "../models/model_state_dict.pt"
+)
