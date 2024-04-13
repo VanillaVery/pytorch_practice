8fbf689324adb4045189989d8755ce4e3ab3783c
yoonyoojin <dbsdbwls0602@naver.com>
2024-04-13 10:39:48 +0900

    데이터셋 분리

--- Chap02_파이토치기초/split_model_dataset.py
+++ Chap02_파이토치기초/split_model_dataset.py
@@ -117,3 +117,92 @@ torch.save(
 torch.save(
     model.state_dict(), "../models/model_state_dict.pt"
 )
+#%%
+"""
+데이터세트 분리
+"""
+import torch
+import pandas as pd
+from torch import nn
+from torch import optim
+from torch.utils.data import Dataset,DataLoader,random_split
+
+#사용자 정의 데이터세트 
+class CustomDataset(Dataset):
+    def __init__(self,file_path):
+        df = pd.read_csv(file_path)
+        self.x = df.iloc[:,0].values
+        self.y = df.iloc[:,1].values
+        self.length = len(df)
+
+    def __getitem__(self, index):
+        x = torch.FloatTensor([self.x[index] **2 , self.x[index]])
+        y = torch.FloatTensor([self.y[index]])
+        return x,y
+    
+    def __len__(self):
+        return self.length
+    
+
+#사용자 정의 모델
+class CustomModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.layer = nn.Linear(2,1)
+
+    def forward(self,x):
+        x = self.layer(x)
+        return x
+    
+
+#사용자 정의 데이터세트 및 데이터로더
+dataset = CustomDataset('../datasets/non_linear.csv')
+dataset_size = len(dataset)
+train_size = int(dataset_size *0.8)
+validation_size = int(dataset_size *0.1)
+test_size = dataset_size - train_size - validation_size
+
+train_dataset , validation_dataset, test_dataset = random_split(dataset, [train_size,validation_size,test_size])
+print(f"Train data size : {len(train_dataset)}")
+print(f"Validataion data size : {len(validation_dataset)}")
+print(f"Test data size : {len(test_dataset)}")
+
+train_dataloader = DataLoader(train_dataset, batch_size=16, shuffle=True, drop_last=True)
+validation_dataloader = DataLoader(validation_dataset, batch_size=4, shuffle=True, drop_last=True)
+test_dataloader = DataLoader(test_dataset, batch_size=4, shuffle=True, drop_last=True)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = CustomModel().to(device)
+criterion = nn.MSELoss().to(device)
+optimizer = optim.SGD(model.parameters(), lr=0.0001)
+
+for epoch in range(10000):
+    cost = 0.0
+
+    for x, y in train_dataloader:
+        x = x.to(device)
+        y = y.to(device)
+
+        output = model(x)
+        loss = criterion(output, y)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        cost += loss
+
+    cost = cost / len(train_dataloader)
+
+    if (epoch +1) % 1000 == 0:
+        print(f"epoch : {epoch +1:4d}, Model: {list(model.parameters())}, Cost: {cost:.3f}")
+
+
+with torch.no_grad():
+    model.eval() # 평가 모드로 반환(특정 계층에서는 평가 모드와 학습 모드가 다름)
+    for x, y in validation_dataloader:
+        outputs = model(x)
+        print(f"X : {x}")
+        print(f"Y : {y}")
+        print(f"Predict : {outputs}")
+        print("=====================")
